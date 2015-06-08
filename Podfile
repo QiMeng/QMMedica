e6917946fc9c2706d4330fe7285a5430e0613a11
QiMENG <616684889@qq.com>
2015-06-08 16:15:18 +0800

    create
    本草纲目

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, "7.0"
+
+pod 'AFNetworking', '~> 2.5.4'
+pod 'GDataXML-HTML', '~> 1.2.0'
+pod 'CocoaLumberjack', '~> 2.0.0'
+pod 'SVProgressHUD', '~> 1.1.3'
+pod 'UMengAnalytics-NO-IDFA', '~> 3.4.7'
+pod 'UMengSocial', '~> 4.2.2'
+pod 'SDWebImage', '~> 3.7.2'
+pod 'iCarousel', '~> 1.8.1'
+pod 'UIImage+BlurredFrame', '~> 0.0.4'
---
+++ QMMedica/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QMMedica
+//
+//  Created by QiMENG on 15/6/8.
+//  Copyright (c) 2015年 QiMENG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
